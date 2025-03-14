31e608ad2eec475c5f885fb95a85361353819c9e
harshitbhalla11 <harshit.bhalla.002@gmail.com>
2025-03-14 15:37:08 +0000

    Fix: Add next poppins font.

--- src/app/components/TicTacProText/TictacproText.tsx
+++ src/app/components/TicTacProText/TictacproText.tsx
@@ -1,14 +1,21 @@
 
 import styles from "./TictacproText.module.scss";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ 
+  subsets: ["latin"], 
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
 
 const TictacproText = () => {
   return (
-    <div className={styles.bottomText}>
+    <div className={`${styles.bottomText} ${poppins.className}`}>
       <div className={styles.primaryText}>tic.</div>
       <div className={styles.secondaryText}>tac.</div>
       <div className={styles.primaryText}>pro.</div>
     </div>
   );
 };
+
 
 export default TictacproText;
